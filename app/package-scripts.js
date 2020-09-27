4e4b98652af40fb8723dc2fe8e5ebb9b41561ba5
Peter Menke <pmenke@hawk.iit.edu>
2020-09-27 14:30:51 -0500

    Initial setup work

---
+++ app/package-scripts.js
@@ -0,0 +1,6 @@
+module.exports = {
+  scripts: {
+    default: 'migration-clean-diff',
+    test: 'echo "Error: no test specified" && exit 1',
+  }
+};
---
+++ package-scripts.js
@@ -0,0 +1,7 @@
+module.exports = {
+  scripts: {
+    lib: 'npm start -C lib',
+    app: 'npm start -C app',
+    test: 'echo "Error: no test specified" && exit 1'
+  }
+};
